1d3bf38db5ff640e428767056027798271fff9a8
Marcos <marcosviny27032005@gmail.com>
2026-06-12 21:24:10 -0300

    primeiro passo da programacao

---
+++ programa01.py
@@ -0,0 +1,12 @@
+print("hello word")
+ultimo_nome = "Silva"
+idade = 21
+altura = 1.71
+peso = 58
+maior_de_idade = True
+print("cavalo salamaleico da silva")
+nome = "Marcos"
+print("Olá ",nome)
+print(f"Olá {nome}")
+print("Olá ", nome, ultimo_nome, "você tem", idade, "anos, sua altura é ", altura, "você é maior de idade", maior_de_idade, "e pesa", peso)
+print(f"Olá {nome} {ultimo_nome} você tem {idade} anos, sua altura {altura}, você pesa {peso}, você é maioir de idade? {maior_de_idade}")
